52bbc3fd0654e5b4fde1f7855da4e36face0f362
Vladislav <v.kirbaba@gmail.com>
2015-09-15 12:05:55 +0300

    Об авторе и фоторама

--- index.php
+++ index.php
@@ -37,6 +37,40 @@
 				вам ваше поражение, – в этом кроется настоящая сила.</p>
     	</div>
     </section>
+
+    <section class="aboutme">
+        <div class="aboutme__text">
+            <div class="aboutme__text--line"></div>
+            <h3>Обо мне</h3>
+            <p>Lorem ipsum dolor sit amet, vel eu illud noluisse gubergren, an sit quidam moderatius, 
+                per at feugiat adipiscing temporibus. Quo ignota eirmod eu, per in saepe eruditi corrumpit. 
+                Vel zril tibique id, vix case docendi et. Duo enim mucius an, per civibus adversarium te. 
+                Eum wisi maiorum fastidii ex, vitae salutatus euripidis usu ea.</p>
+                <p>Lorem ipsum dolor sit amet, vel eu illud noluisse gubergren, an sit quidam moderatius, 
+                    per at feugiat adipiscing temporibus. Quo ignota eirmod eu, per in saepe eruditi 
+                    corrumpit. Vel zril tibique id, </p>
+            <a href="#">узнать больше</a>
+        </div>
+        <div class="aboutme__photo">
+            <div class="aboutme__photo--front">
+                <img src="<?php bloginfo('template_directory'); ?>/img/_MG_0396.png" alt="">
+            </div>
+            <div class="aboutme__photo--back">
+                <img src="<?php bloginfo('template_directory'); ?>/img/back.png" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="services">
+        <div class="fotorama" data-minwidth="100%" data-fit="cover" data-autoplay="true" >
+            <div class="contain">
+                <div class=""><h1>Hello World</h1></div>
+            </div>
+            <div class="contain">
+                <div class=""><h1>Hello World</h1></div>
+            </div>
+        </div>
+    </section>
 <?php wp_footer(); ?>
 </body>
 </html>
