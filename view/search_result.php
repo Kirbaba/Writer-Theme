f3093a221da995ab5c32c71d30d6b345259a6e2d
Kirill <apuc06@mail.ru>
2015-10-05 16:35:21 +0300

    post store

--- view/search_result.php
+++ view/search_result.php
@@ -2,9 +2,10 @@
 <?php foreach($result['posts'] as $post) { ?>
     <?php $type = 'Запись'; ?>
     <?php if($post->post_type == 'topic') {$type = 'Форум';} ?>
-    <?php if($post->post_type == 'topic' or $post->post_type == 'post') { ?>
+    <?php if($post->post_type == 'store') {$type = 'Магазин';} ?>
+    <?php if($post->post_type == 'topic' or $post->post_type == 'post' or $post->post_type == 'store') { ?>
     <div class="searchItem">
-        <div class="searchItemTitle"><?= $post->post_title ?> (<?= $type ?>)</div>
+        <div class="searchItemTitle"><a href="<?= $post->guid ?>"><?= $post->post_title ?> (<?= $type ?>)</a></div>
         <div class="searchItemText">
             <?= $post->post_content; ?>
         </div>
