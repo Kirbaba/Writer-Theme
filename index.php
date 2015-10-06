ef444fec37649fa9fef4b65dc7ece50ac4b5cdfd
morozovigor <perffectgame@gmail.com>
2015-10-06 11:15:21 +0300

    add cart link

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     			<li><a href="<?php echo get_permalink(23); ?>">форум</a></li>
     			<li><a href="#go_contacts" class="smoothScroll">КОНТАКТЫ</a></li>    			
     		</ul>
-    		<a href="#nowhere" class="button-buy"></a>
+    		<a href="/cart" class="button-buy"></a>
 			<a href="/search/" class="button-search"></a>
     	</nav>
     </header>
